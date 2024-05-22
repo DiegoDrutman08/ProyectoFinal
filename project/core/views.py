d606ffc024274789971593a24c88b631bd2497d0
DiegoDrutman <diego.drutman@gmail.com>
2024-05-22 00:22:10 -0300

    readme & register

--- project/core/views.py
+++ project/core/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from .forms import CustomAuthenticationForm, CustomUserCreationForm
 
 
@@ -15,12 +15,18 @@ class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = "core/login.html"
 
+
 def register(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return render(request, "core/index.html", {"mensaje": "Usuario creado"})
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('core:home')  # Redirige a la página principal
     else:
         form = CustomUserCreationForm()
     return render(request, "core/register.html", {"form": form})
